bd4173bdec0ed44e990419d2e67a3d6c1d62fa6a
faisoabdirisak <faisoabdirisak@gmail.com>
2022-03-07 17:43:07 +0300

    added comment model

--- clone/models.py
+++ clone/models.py
@@ -7,11 +7,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
-
+class Comment(models.Model):
+    name= models.CharField(max_length=100, null=False, blank=False)
+   
+	
+	
+    def __str__(self):
+        return self.name
 
   
 
