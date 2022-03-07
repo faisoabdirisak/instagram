84b4e53083182539aa75f0ea6c389976efb51f9b
faisoabdirisak <faisoabdirisak@gmail.com>
2022-03-07 09:53:32 +0300

    deleted

--- clone/models.py
+++ clone/models.py
@@ -13,18 +13,6 @@ class Profile(models.Model):
 
 
 
-    #  # resizing images
-    # def save(self, *args, **kwargs):
-    #     super().save()
-
-    #     img = Image.open(self.avatar.path)
-
-    #     if img.height > 100 or img.width > 100:
-    #         new_img = (100, 100)
-    #         img.thumbnail(new_img)
-    #         img.save(self.avatar.path)
-
-    # def __str__(self):
-    #     return self.user.username
+  
 
         
