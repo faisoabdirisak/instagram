c5dc05916c5783098b2c223aaa3a25cce61053a3
faisoabdirisak <faisoabdirisak@gmail.com>
2022-03-08 14:08:42 +0300

    add

--- instagram/settings.py
+++ instagram/settings.py
@@ -106,12 +106,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'instagram.wsgi.application'
 
-
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# Email configurations remember to install python-decouple
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'faisoabdirisak@gmail.com'
+EMAIL_HOST_PASSWORD = 'faska12'
+EMAIL_HIST_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # Database
